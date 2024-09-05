0a73944b9f9e6410a4f9523d8d5090ce7ee28279
code1man <149554589+code1man@users.noreply.github.com>
2024-09-05 10:59:39 +0800

    1、更新database工具类

--- src/main/java/org/example/demo/Main.java
+++ src/main/java/org/example/demo/Main.java
@@ -8,8 +8,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main extends Application {
 
     private static Scene scene;
@@ -23,7 +21,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 
     public static void setRoot(String fxml) throws IOException {
--- src/main/java/org/example/demo/utils/DbUtil.java
+++ src/main/java/org/example/demo/utils/DbUtil.java
@@ -1,24 +1,24 @@
 package org.example.demo.utils;
 
 //数据库相关工具类
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+
+import java.sql.*;
+import java.util.ArrayList;
 
 //Connection con 连接对象
 public class DbUtil {
-    private static Connection con; // 声明Connection对象
-    private static PreparedStatement preparedStatement;
+    private static Connection con = null; // 声明Connection对象
+    private static PreparedStatement preparedStatement = null;
+    private static ResultSet resultSet = null;
 
     public DbUtil() {
     }
 
     // 建立返回值为Connection的方法
-    public static Connection getConnection(String user, String password, String dataBaseName) {
+    public static Connection getConnection() {
         try { // 通过访问数据库的URL获取数据库连接对象
             con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/" + dataBaseName + "?useUnicode=true&characterEncoding=gbk", user, password);
+                    "jdbc:mysql://localhost:3306/" + "sys" + "?useUnicode=true&characterEncoding=gbk", "root", "123456");
             System.out.println("数据库连接成功");
         } catch (SQLException e) {
             System.out.println("数据库连接失败");
@@ -26,13 +26,66 @@ public class DbUtil {
         return con;
     }
 
+    //获取命令对象
     public static PreparedStatement getpreparedStatement(String sql) {
         try {
-            con = getConnection("root", "zkd2621023939", "sys");
+            con = getConnection();
             preparedStatement = con.prepareStatement(sql);
         } catch (SQLException e) {
             System.out.println("sql语法错误");
         }
         return preparedStatement;
+    }
+
+    // 返回受影响的行数
+    public static int executeUpdate(String sql, ArrayList<Object> params) {
+        try {
+            preparedStatement = getpreparedStatement(sql);
+            for (int i = 0; i < params.size(); i++) {
+                preparedStatement.setObject(i + 1, params.get(i));
+            }
+            return preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static ResultSet executeQuery(String sql, ArrayList<Object> params) {
+        getpreparedStatement(sql);
+        try {
+            for (int i = 0; i < params.size(); i++) {
+                preparedStatement.setObject(i + 1, params.get(i));
+            }
+            resultSet = preparedStatement.executeQuery();
+            return resultSet;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void close() {
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if (preparedStatement != null) {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
